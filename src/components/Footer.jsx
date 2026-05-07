44304610ecf7bbbe21b3d1a681f64cb3a1778437
webmancer1 <mwendwajames2004@gmail.com>
2026-05-07 12:50:49 +0300

    Footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -8,10 +8,11 @@ const Footer = () => {
     <footer className="footer border-t border-glass">
       <div className="container flex flex-col items-center justify-center gap-4">
         <a href="#" className="brand">
-          <span className="brand-text">James</span><span className="brand-dot">.</span>
+          <span className="brand-text"></span>
+          <span className="brand-dot"></span>
         </a>
         <p className="text-muted text-sm text-center">
-          &copy; {year} James Mwendwa. All rights reserved. <br/>
+          &copy; {year} James Mwendwa. All rights reserved. <br />
           Built with React & Vanilla CSS.
         </p>
       </div>

f08887522ef21e3a6dc02caec78847525870a184
webmancer1 <mwendwajames2004@gmail.com>
2026-05-08 10:43:27 +0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,7 @@ const Navbar = () => {
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
     { name: "Experience", href: "#experience" },
+    { name: "Blog", href: "#blog" },
     { name: "Contact", href: "#contact" },
   ];
 

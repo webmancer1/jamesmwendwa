32bc13ea6a49d6dc25cbd992a8eb883a0efb535b
webmancer1 <mwendwajames2004@gmail.com>
2026-05-08 10:13:07 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
+import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -18,6 +19,7 @@ function App() {
         <Skills />
         <Projects />
         <Experience />
+        <Blog />
         <Contact />
       </main>
       <Footer />
